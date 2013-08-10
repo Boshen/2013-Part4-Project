8fc42478892b3564a2df833538d08b1257799251
Boshen <BoshenC@gmail.com>
2013-08-10 23:14:34 +1200

    updated results for Terrassa

--- Draw/pq_runtime.py
+++ Draw/pq_runtime.py
@@ -1,11 +1,12 @@
 import pylab
 from matplotlib.font_manager import FontProperties
 
-x = ['Fibonacci', 'Binary', 'Ternay', 'Skew', 'Pairing', 'Binomial', 'STL\n Heap', 'STL Binary\nSearch Tree']
+x = ['Binomial', 'Fibonacci', 'Binary', 'Ternary', 'Pairing', 'Skew', 'STL Binary\nSearch Tree', 'STL\n Heap']
 
-y = [155.14, 130.22, 155.34, 120.45, 140.55, 369.10, 87.52, 149.95]
+#y = [369.10, 155.14, 130.22, 155.34, 140.55, 120.45, 149.95, 87.52]
+y = [165.8, 73.66, 56.94, 62.21, 69.83, 52.57, 49.95, 34.89]
 
-y, x = zip(*sorted(zip(y,x), reverse=True))
+#y, x = zip(*sorted(zip(y,x), reverse=True))
 
 font = FontProperties()
 font.set_family('serif')
@@ -16,8 +17,9 @@ index = range(len(x))
 pylab.bar(index, y, width=0.5, align='center')
 pylab.xticks(index, x, size='small', fontproperties=font)
 
-pylab.title('Dijkstra\'s algorithim run times using different priority queues on ChicagoSketch', fontproperties=font)
+#pylab.title('Dijkstra\'s algorithim run times using different priority queues on ChicagoSketch', fontproperties=font)
+pylab.title('Dijkstra\'s algorithim run times using different priority queues on Winnipeg', fontproperties=font)
 pylab.xlabel('Priority Queues', fontproperties=font)
 pylab.ylabel('Run time\n(seconds)', {'rotation':'horizontal'}, fontproperties=font)
 
-pylab.savefig('pq_runtime.pdf')
+pylab.savefig('pq_runtime2.pdf')
---
+++ Draw/random_time.py
@@ -0,0 +1,77 @@
+import pylab
+from matplotlib.font_manager import FontProperties
+from matplotlib.backends.backend_pdf import PdfPages
+
+pdf = PdfPages('random_time.pdf')
+font = FontProperties()
+font.set_family('serif')
+
+#--------------------------------------------- 
+fig = pylab.figure(figsize=(9,5), dpi=200)
+x = [0, 0.3, 0.4, 0.5, 0.6, 0.7]
+y = [126.92, 105.73, 86.8, 73.12, 82.46, 86.6]
+iters = [415, 409, 408, 396, 507, 620]
+index = range(len(x))
+pylab.bar(index, y, width=0.5, align='center')
+pylab.xticks(index, x, size='small', fontproperties=font)
+
+for i, (xx,yy) in enumerate(zip(index,y)):
+    pylab.annotate('%d iters' % iters[i], xy=(xx, yy), xytext=(0,2), textcoords='offset points', ha='center', va='bottom', fontproperties=font)
+
+pylab.title('Astar search with skipping calculation randomly on Terrassa', fontproperties=font)
+pylab.xlabel('Probability of skipping a calculation', fontproperties=font)
+pylab.ylabel('Run time\n(seconds)', {'rotation':'horizontal'}, fontproperties=font)
+pdf.savefig(fig, bbox_inches='tight')
+
+#--------------------------------------------- 
+fig = pylab.figure(figsize=(9,5), dpi=200)
+x = [0, 0.3, 0.4, 0.5, 0.6, 0.7]
+y = [19.53, 18.71, 17.48, 18.18, 17.42, 16.53]
+iters = [26, 25, 26, 30, 31, 34]
+index = range(len(x))
+pylab.bar(index, y, width=0.5, align='center')
+pylab.xticks(index, x, size='small', fontproperties=font)
+
+for i, (xx,yy) in enumerate(zip(index,y)):
+    pylab.annotate('%d iters' % iters[i], xy=(xx, yy), xytext=(0,2), textcoords='offset points', ha='center', va='bottom', fontproperties=font)
+
+pylab.title('Astar search with skipping calculation randomly on ChicagoSketch', fontproperties=font)
+pylab.xlabel('Probability of skipping a calculation', fontproperties=font)
+pylab.ylabel('Run time\n(seconds)', {'rotation':'horizontal'}, fontproperties=font)
+pdf.savefig(fig, bbox_inches='tight')
+
+#--------------------------------------------- 
+fig = pylab.figure(figsize=(9,5), dpi=200)
+x = ['none', 15, 25, 50, 100, 200]
+y = [126.92, 68.48, 61.60, 56.52, 50.82, 59.21]
+iters = [415, 351, 355, 379, 411, 563]
+index = range(len(x))
+pylab.bar(index, y, width=0.5, align='center')
+pylab.xticks(index, x, size='small', fontproperties=font)
+
+for i, (xx,yy) in enumerate(zip(index,y)):
+    pylab.annotate('%s iters' % str(iters[i]), xy=(xx, yy), xytext=(0,2), textcoords='offset points', ha='center', va='bottom', fontproperties=font)
+
+pylab.title('Astar search with skipping calculation if previous 2 is not changed on Terrassa', fontproperties=font)
+pylab.xlabel('Skip next n calculation', fontproperties=font)
+pylab.ylabel('Run time\n(seconds)', {'rotation':'horizontal'}, fontproperties=font)
+pdf.savefig(fig, bbox_inches='tight')
+
+#--------------------------------------------- 
+fig = pylab.figure(figsize=(9,5), dpi=200)
+x = ['none', 5, 10, 15, 20]
+y = [19.53, 16.14, 16.03, 16.46, 17.94]
+iters = [26, 26, 35, 42, 53]
+index = range(len(x))
+pylab.bar(index, y, width=0.5, align='center')
+pylab.xticks(index, x, size='small', fontproperties=font)
+
+for i, (xx,yy) in enumerate(zip(index,y)):
+    pylab.annotate('%s iters' % str(iters[i]), xy=(xx, yy), xytext=(0,2), textcoords='offset points', ha='center', va='bottom', fontproperties=font)
+
+pylab.title('Astar search with skipping calculation if previous 2 is not changed on ChicagoSketch', fontproperties=font)
+pylab.xlabel('Skip next n calculation', fontproperties=font)
+pylab.ylabel('Run time\n(seconds)', {'rotation':'horizontal'}, fontproperties=font)
+pdf.savefig(fig, bbox_inches='tight')
+
+pdf.close()
